ec862b6a4208e5509f1ec006a9673f152458b158
DavLin <davlin@creatorise.com>
2021-10-03 16:14:29 +0200

    Remove all validation

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-const validate = require('./logic/validate_rules');
 const fabricate_words = require('./logic/fabricate_words');
 
 module.exports = word_fabricator_config;
 
 function word_fabricator_config(initial_rules) {
-    const valid_initial_rules = validate.initial_rules(initial_rules);
-
     return function word_fabricator(additional_rules) {
-        const valid_additional_rules = validate.additional_rules(additional_rules);
-        const valid_rules = { ...valid_initial_rules, ...valid_additional_rules };
+        const rules = { ...initial_rules, ...additional_rules };
 
-        const { blueprint, max_length, initial_chars } = valid_rules;
+        const { blueprint, max_length, initial_chars } = rules;
         const initial_parts = initial_chars;
 
         return fabricate_words(blueprint, max_length, initial_parts);
